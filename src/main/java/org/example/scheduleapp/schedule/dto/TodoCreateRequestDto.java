46583281d7fe23a4171be560746aab3e7a557e7a
dornrrl0932 <dornrrl0932@naver.com>
2024-12-18 02:19:17 +0900

    schedule.controller,service,repository 패키지 생성

--- src/main/java/org/example/scheduleapp/schedule/dto/TodoCreateRequestDto.java
+++ src/main/java/org/example/scheduleapp/schedule/dto/TodoCreateRequestDto.java
@@ -1,8 +1,19 @@
 package org.example.scheduleapp.schedule.dto;
 
+import jakarta.persistence.Column;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
 /*
-할 일 생성 완료 시 응답할 데이터
+할 일 생성 시 요청받을 데이터
  */
+@Getter
 public class TodoCreateRequestDto {
+
+    private String title;
+    private String workTodo;
 
 }
--- src/main/java/org/example/scheduleapp/schedule/dto/TodoCreateResponseDto.java
+++ src/main/java/org/example/scheduleapp/schedule/dto/TodoCreateResponseDto.java
@@ -1,9 +1,21 @@
 package org.example.scheduleapp.schedule.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 /*
-할 일 생성 시 요청받을 데이터
+할 일 생성 완료 시 응답할 데이터
  */
+@Getter
+@AllArgsConstructor // 전체 필드를 가진 생성자 추가ㄴ
 public class TodoCreateResponseDto {
+
+    private Long id;
+    private String writer;
     private String title;
     private String workTodo;
+    private LocalDateTime createDateTime;
+    private LocalDateTime modifyDateTime;
 }
