d7ad5d31c3e68c2eefc21034d81df558d91c3c80
dornrrl0932 <dornrrl0932@naver.com>
2024-12-17 16:21:15 +0900

    ScheduleAppApplication 클래스에 @EnableJpaAuditing 적용

--- src/main/java/org/example/scheduleapp/ScheduleAppApplication.java
+++ src/main/java/org/example/scheduleapp/ScheduleAppApplication.java
@@ -2,7 +2,9 @@ package org.example.scheduleapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing // JPA Auditing을 사용하기 위해 애플리케이션 클래스 상단에 선언
 @SpringBootApplication
 public class ScheduleAppApplication {
 
