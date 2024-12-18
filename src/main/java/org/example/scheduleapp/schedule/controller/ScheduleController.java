775491088602a27c4277010e28a846e6cbf04b73
dornrrl0932 <dornrrl0932@naver.com>
2024-12-18 09:31:06 +0900

    ScheduleController 할 일 생성 API 구현

--- src/main/java/org/example/scheduleapp/schedule/controller/ScheduleController.java
+++ src/main/java/org/example/scheduleapp/schedule/controller/ScheduleController.java
@@ -1,16 +1,38 @@
 package org.example.scheduleapp.schedule.controller;
 
+import org.example.scheduleapp.schedule.dto.TodoCreateRequestDto;
+import org.example.scheduleapp.schedule.dto.TodoCreateResponseDto;
 import org.example.scheduleapp.schedule.servise.ScheduleServise;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/todos")
 public class ScheduleController {
-    //속
-    private ScheduleServise scheduleServise;
 
-    //생
+    //속성
+    private final ScheduleServise scheduleServise;
 
-    //기
+    /*
+    생성자
+    ScheduleServise 사용하기 위한 생성자 주입
+     */
+    public ScheduleController(ScheduleServise scheduleServise) {
+        this.scheduleServise = scheduleServise;
+    }
+
+    //기능
+
+    /*
+    할 일 생성
+     */
+    @PostMapping
+    public ResponseEntity<TodoCreateRequestDto>createTodo (@RequestBody TodoCreateRequestDto requestDto) {
+
+        return new ResponseEntity<>(scheduleServise.createTodo(requestDto), HttpStatus.CREATED);
+    }
 }
