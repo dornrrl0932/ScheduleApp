d3f8613f4e0e66b5d9f602df00775759afa90dac
dornrrl0932 <dornrrl0932@naver.com>
2024-12-19 11:15:08 +0900

    UserResponseDto 설계

---
+++ src/main/java/org/example/scheduleapp/schedule/dto/UserResponseDto.java
@@ -0,0 +1,16 @@
+package org.example.scheduleapp.schedule.dto;
+
+import lombok.Getter;
+
+@Getter
+public class UserResponseDto {
+
+    private final String userName;
+    private final String email;
+
+    public UserResponseDto(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+    }
+}
+
